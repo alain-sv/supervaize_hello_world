ba79cc8dc1ac6e4ea9f5d7dd5c4cf95e560cd687
Alain Prasquier <alain@supervaize.com>
2026-02-04 15:18:24 +0200

    Update logging format in job_start function of agent_simple.py for improved readability
    - Modified debug and print statements to include a clearer prefix for better identification during job execution.

--- agent_simple.py
+++ agent_simple.py
@@ -125,8 +125,8 @@ def job_start(**kwargs) -> JobResponse | None:
     """
 
     # Get main job field:
-    log.debug(f"AGENT ExampleAgent: Job Fields : {job_fields}")
-    print(f"PRINT - DEBUG AGENT ExampleAgent: Job Fields : {job_fields}")
+    log.debug(f"*-*-*-*-*-*-*-AGENT ExampleAgent: Job Fields : {job_fields}")
+    print(f"*-*-*-*-*- PRINT - DEBUG AGENT ExampleAgent: Job Fields : {job_fields}")
     how_many_times_to_say_hello = int(job_fields.get("How many times to say hello"))
 
     for i in range(how_many_times_to_say_hello):
